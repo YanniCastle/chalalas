5615b34e0a790a3152a16d70795601d7ba71b219
Yanni Castle <yannicastle5@gmail.com>
2023-09-17 23:44:41 -0600

    ignorar9

--- conexion.php
+++ conexion.php
@@ -1,3 +1,4 @@
+<!--No debe subirse-->
 <?php 
 $conn = new mysqli("localhost","root","","chalalas");//agrege de recuperar
 //Usaremos libreria PDO
--- config.php
+++ config.php
@@ -1,3 +1,4 @@
+<!--No debe subirse-->
 <?php
 
 $Chost = "localhost";
