d60583a0f8679fcfe56004297beb302edbd28422
Yanni Castle <yannicastle5@gmail.com>
2023-09-24 03:19:37 -0600

    add recuperacion2, recupear2

--- login.php
+++ login.php
@@ -61,7 +61,7 @@
     <input type="submit" value="Iniciar Sesión">
     <tr>
       <td>
-        <a href="recuperacion.php">¿Olvidaste tu contraseña?</a>
+        <a href="recuperacion2.php">¿Olvidaste tu contraseña?</a>
       </td>
     </tr>
   </form>
---
+++ recuperacion2.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+     <title>Recuperacion2</title>
+     <link rel="shortcut icon" href="letraCfondonegro.png">
+  <link rel="stylesheet" href="style3.css" /><!--fondo-->
+  <link rel="stylesheet" href="stylefoto2.css" /><!--menu-->
+</head>
+<body>
+    <script>
+    var x = document.getElementById("frmlogin");
+    var y = document.getElementById("frmregistrar");
+    var z = document.getElementById("btnvai");
+	var textcolor1=document.getElementById("vaibtnlogin");
+	var textcolor2=document.getElementById("vaibtnregistrar");
+	textcolor1.style.color="white";
+	textcolor2.style.color="black";
+
+function cancelarform() {
+  document.getElementById("formrecuperar").style.display = "none";
+}
+    </script>
+<div class="caja_popup" id="formrecuperar">
+  <form action="recuperar2.php" class="contenedor_popup" method="POST">
+        <table>
+		<tr><th colspan="2">Recuperacion2 contraseña</th></tr>
+            <tr> 
+                <td><b>&#128231; Correo</b></td>
+                <td><input type="email" name="txtcorreo" required class="cajaentradatexto"></td>
+            </tr>
+            <tr> 	
+               <td colspan="2">
+				   <button type="button" onclick="cancelarform()" class="txtrecuperar">Cancelar</button>
+				   <input class="txtrecuperar" type="submit" name="btnrecuperar" value="Enviar" onClick="javascript: return confirm('¿Deseas enviar tu contraseña a tu correo?');">
+			</td>
+            </tr>
+        </table>
+    </form>
+	</div>
+</body>
+</html>
