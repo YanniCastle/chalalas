c7b89d07b81234efcbe2646079d07b8b064f3324
Yanni Castle <yannicastle5@gmail.com>
2023-10-07 05:01:39 -0600

    subir a hostinger

---
+++ pruebasubir.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>prueba subir</title>
+</head>
+<body>
+  <h1>Prueba subir a hostinger</h1>
+  
+</body>
+</html>
