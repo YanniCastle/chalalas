40e30704088910ab4d3343a46ab2610c847be636
Yanni Castle <yannicastle5@gmail.com>
2023-10-08 18:40:35 -0600

    videos

--- videos.php
+++ videos.php
@@ -4,75 +4,61 @@
 <head>
   <meta charset="utf-8">
   <title>Videos</title>
-  <link rel="stylesheet" href="stylefoto2.css" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style1b.css" /><!--barra de menu plegable-->
+  <script src="a2dd6045c4.js" crossorigin="anonymous"></script><!--js para iconos-->
+  <link rel="stylesheet" type="text/css" href="estilos.css"><!--Iconos-->
   <link rel="shortcut icon" href="letraCfondonegro.png">
 </head>
 
 <body>
-  <br>
-<article id="position">
-   <nav>
-    <ul>
-      <li>
-        <a href="usuarios_registrados1.php"><img src="imagenes/chalalas4.png"></a>
-      </li>
-      <li>
-        <a href="#">Productos</a>
-        <ul>
-          <li><a href="comprar.php">Comprar</a></li>
-          <li><a href="vender.php">Vender</a></li>
-        </ul>
-      </li>
-
-      <li>
-        <a href="#">Servicios</a>
-        <ul>
-          <li><a href="buscar.php">buscar</a></li>
-          <li><a href="ofrecer.php">ofrecer</a></li>
-        </ul>
-      </li>
-
-      <li><a href="#">Categorias</a>
-        <ul>
-          <li><a href="#">categoria 1</a></li>
-          <li><a href="#">categoria 2</a></li>
-          <li><a href="#">categoria 3</a></li>
-          <li><a href="#">categoria 4</a></li>
-        </ul>
-      </li>
-
-      <li><a href="Formulario.php">Comentarios</a></li>
-      <li><a href="fotos.php">Fotos</a></li>
-      <li><a href="videos.php">videos</a></li>
-
-      <li><a href="#">Contactos</a>
-        <ul>
-          <li><a href="#">Whats App</a></li>
-          <li><a href="email.php">Email</a></li>
-        </ul>
-      </li>
-      <li><a href="muro.php">Muro</a></li>
-      <li><a href="cierre.php">cerrar sesion</a></li>
-    </ul>
-  </nav>
-   </article>
+  <article id="position">
+    <header>
+      <div class="ocultar-div">
+        <a><img src="chalalas2.png"></a>
+      </div>
+      <div class="ocultar-div2">
+        <a><img src="imagenes/chalalas4.png"></a>
+      </div>
+      <a>
+        <form action="" method="get">
+          <input type="search" id="busqueda" name="busqueda" required placeholder="¿Que artículo buscas?">
+          <input type="submit" id="enviar" name="enviar" value="Buscar"><br><br>
+        </form>
+      </a>
+      <input type="checkbox" id="check">
+      <label for="check" class="mostrar-menu">
+        &#8801<!--hamburguesa-->
+      </label>
+      <nav class="menu">
+        <a href="subir_imagenes.php"><i class="fa-solid fa-shop"></i></a>
+        <a href="Formulario.php"><i class="fa-regular fa-comment"></i></a>
+        <a href="crud11.php"><i class="fa-regular fa-image"></i></a>
+        <a href="videos.php"><i class="fa-solid fa-video"></i></a>
+        <a href="">usuarios</a><!--privada online-->
+        <a href=""><i class="fa-solid fa-user"></i></a>
+        <a href="crud4.php"><i class="fa-solid fa-key"></i></a>
+        <a href="muro.php">Muro</a>
+        <a href="cierre.php">cerrar sesion</a>
+        <label for="check" class="esconder-menu">
+          &#215 <!--la x-->
+        </label>
+      </nav>
+    </header>
+  </article>
   <?php
   session_start();
   if (!isset($_SESSION["usuario"])) {
     header("location:login.php");
   }
   ?>
+  <br><br>
   <h1>Videos</h1>
   <?php
   echo "<h2>¡Hola, " . $_SESSION["usuario"] . "!<br></h2>";
   ?>
   <video src="Smooth.mp4" controls width="420" height="240"></video>
-  <video src="Smooth.mp4" controls width="420" height="240"></video>
-  <video src="Smooth.mp4" controls width="420" height="240"></video>
-  <video src="Smooth.mp4" controls width="420" height="240"></video>
-  <video src="Smooth.mp4" controls width="420" height="240"></video>
-  <video src="Smooth.mp4" controls width="420" height="240"></video>
-  
+
 </body>
 
 </html>
