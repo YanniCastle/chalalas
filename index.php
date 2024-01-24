616fd578c45d81271ea9543c5ea0918599bf7be9
Yanni Castle <yannicastle5@gmail.com>
2024-01-24 13:07:28 -0600

    <<a

--- index.php
+++ index.php
@@ -179,7 +179,6 @@
       <img alt="photo 13" src="img/IMG_20230603_023752_895.jpg" />
       <img alt="photo 14" src="img/IMG_20230626_201617_060.jpg" />
       <a href="proximamente.php"><img alt="photo 15" src="img/lampara_amarilla_3.jpg" />
-        << /a>
     </div>
 
     <div class="column">
