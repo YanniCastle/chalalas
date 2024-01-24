e16ef424caccda84fe3688dfa3213cd6eb5336c4
Yanni Castle <yannicastle5@gmail.com>
2024-01-24 14:00:10 -0600

    icono-proximamente

--- proximamente.php
+++ proximamente.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Proximamente...</title>
+  <link rel="shortcut icon" href="letraCfondonegro.png"><!--icono-navegador-->
   <link rel="stylesheet" href="proximamente.css" />
 </head>
 
