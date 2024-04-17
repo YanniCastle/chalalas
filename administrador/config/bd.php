736dfbe6bdf57925fb2c3abed25c9e23c418ec62
Yanni Castle <yannicastle5@gmail.com>
2024-04-17 02:24:38 -0600

    probando cambio en ignore

--- administrador/config/bd.php
+++ administrador/config/bd.php
@@ -1,9 +1,8 @@
 <?php
-$host = "localhost";//solo produccion
+$host = "localhost";
 $bd = "pruebas";
 $usuario = "root";
 $contrasenia = "";
-//01:35:12   //01:43:32
 try { //PDO me comunica directamente con BD  01:36:10
   $conexion = new PDO("mysql:host=$host; dbname=$bd", $usuario, $contrasenia);
   if ($conexion) ;//echo "Conectado... a sitio<br/>";
