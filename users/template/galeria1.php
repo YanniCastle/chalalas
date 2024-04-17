a131e865549d12d059a1556a89b2be65d803515f
Yanni Castle <yannicastle5@gmail.com>
2024-04-17 02:48:30 -0600

    reordenand2 2

---
+++ users/template/galeria1.php
@@ -0,0 +1,38 @@
+<head>
+  <!--Con esta direccion funciona con su archivo en el mismo template-->
+  <link rel="stylesheet" type="text/css" href="users/template/galeria1.css" media="screen">
+</head>
+<!--Galeria-->
+<div class="row">
+  <div class="column">
+    <a href="proximamente.php"><img class="galeria" alt="photo 1" src="galeria_index/cinta_canela_c.jpg" />
+      <a href="proximamente.php"> <img class="galeria" alt="photo 2" src="galeria_index/mouse_c.jpg" />
+        <a href="proximamente.php"> <img class="galeria" alt="photo 3" src="galeria_index/ram_c.jpg" />
+          <a href="proximamente.php"> <img class="galeria" alt="photo 4" src="galeria_index/teclado_c.jpg" />
+            <a href="proximamente.php"> <img class="galeria" alt="photo 5" src="galeria_index/no_break_c.jpg" />
+  </div>
+
+  <div class="column">
+    <a href="proximamente.php"> <img class="galeria" alt="photo 6" src="galeria_index/bicicleta_c.jpg" />
+      <a href="proximamente.php"> <img class="galeria" alt="photo 7" src="galeria_index/extractor_c.jpg" />
+        <a href="proximamente.php"> <img class="galeria" alt="photo 8" src="galeria_index/laptop_HP_c.jpg" />
+          <a href="proximamente.php"> <img class="galeria" alt="photo 9" src="galeria_index/monitores_c.jpg" />
+            <a href="proximamente.php"> <img class="galeria" alt="photo 10" src="galeria_index/multimetro_c.jpg" />
+  </div>
+
+  <div class="column">
+    <a href="proximamente.php"> <img class="galeria" alt="photo 11" src="galeria_index/cobijas_120_c.jpeg" />
+      <a href="proximamente.php"> <img class="galeria" alt="photo 12" src="galeria_index/impermiable_c.jpg" />
+        <a href="proximamente.php"> <img class="galeria" alt="photo 13" src="galeria_index/pluma_negra_c.jpg" />
+          <a href="proximamente.php"> <img class="galeria" alt="photo 14" src="galeria_index/velocimetro_c.jpg" />
+            <a href="proximamente.php"> <img class="galeria" alt="photo 15" src="galeria_index/cinta_canela_c.jpg" />
+  </div>
+
+  <div class="column">
+    <a href="proximamente.php"> <img class="galeria" alt="photo 16" src="galeria_index/cinta_canela_c.jpg" />
+      <a href="proximamente.php"> <img class="galeria" alt="photo 17" src="galeria_index/cinta_canela_c.jpg" />
+        <a href="proximamente.php"> <img class="galeria" alt="photo 18" src="galeria_index/cinta_canela_c.jpg" />
+          <a href="proximamente.php"> <img class="galeria" alt="photo 19" src="galeria_index/cinta_canela_c.jpg" />
+            <a href="proximamente.php"><img class="galeria" alt="photo 20" src="galeria_index/cinta_canela_c.jpg" /></a>
+  </div>
+</div>
