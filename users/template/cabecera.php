d034a550bad77cb84ef85d2e81dd1eb388626c2d
Yanni Castle <yannicastle5@gmail.com>
2024-04-25 17:30:14 -0600

    ibbb

--- users/template/cabecera.php
+++ users/template/cabecera.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Chalalas.com</title>
-  <link rel="stylesheet" href="style1bb.css" />
+  <link rel="stylesheet" href="style1bbb.css" />
   <!--barra de menu plegable-->
 
   <script src="a2dd6045c4.js" crossorigin="anonymous"></script>
