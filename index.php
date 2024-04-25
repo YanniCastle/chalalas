789cca024e51e0716f7f03fc40e0d6f934c2dae7
Yanni Castle <yannicastle5@gmail.com>
2024-04-25 17:08:54 -0600

    index_old

--- index.php
+++ index.php
@@ -1,136 +1,17 @@
-<!DOCTYPE html>
-<html lang="es">
-<!--Trabajar con index2 para mejorar el resposive moto C -->
+<?php include("template/cabeceramenu.php"); //Aqui esta el buscador y el menu
+?>
 
-<head>
-  <meta charset="UTF-8">
-  <!--Así se evita que la ñ o las tildes no se muestren correctamente-->
-  <meta charset="ISO-8859-1">
-  <!--caracteres para el alfabeto latino-->
-  <meta name="description" content="Sitio web de comercio electrónico, compra/venta de artículos de ocasión">
-  <meta name="keywords" content="articulo usado, compra ocasional, seminuevo">
-  <!--palabras clave-->
-  <meta name="author" content="Chalalas.com">
-  <meta name="date" content="2023-02-20">
-  <meta name="robots" content="index, follow"><!-- sigan los enlaces de una página-->
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Chalalas.com</title>
-  <link rel="shortcut icon" href="letraCfondonegro.png">
-  <!--icono-navegador-->
-  <link rel="stylesheet" type="text/css" href="style13e.css">
-  <!--galeria-->
-  <link rel="stylesheet" type="text/css" href="mobile_index4d.css" media="screen">
-  <link rel="stylesheet" type="text/css" href="index2c.css" media="screen">
-</head>
 
-<body>
-  <header id="position1">
-    <!--desktop-->
-    <div class="chalalas2">
-      <a><img src="chalalas2.png"></a>
-    </div>
-    <a>
-      <form action="index_search.php" method="get">
-        <input type="search" id="busqueda1" name="busqueda" required placeholder="¿Que artículo buscas?">
-        <input type="submit" id="enviar1" name="enviar" value="Buscar"><br><br>
-      </form>
-    </a>
-    <a>
-      <!--Formulario_Insertar_Usuarios3.php-->
-      <form action="Formulario_Insertar_Usuarios3.php">
-        <input type="submit" id="registrate1" name="registrate" value="Registrate"><br><br>
-      </form>
-    </a>
-    <a>
-      <form action="login.php">
-        <input type="submit" id="login1" name="login" value="Iniciar Sesión"><br><br>
-      </form>
-    </a>
-  </header>
-  <!--/////////////////////////////////////////////////////////-->
-  <header id="position2">
-    <!--moto C, en vertical-->
-    <tr>
-      <td>
-        <div class="chalalas4">
-          <img src="imagenes/chalalas4.png">
-        </div>
-      </td>
-      <td>
-        <!--Formulario_Insertar_Usuarios3.php-->
-        <form action="Formulario_Insertar_Usuarios3.php">
-          <input type="submit" id="registrate2" name="registrate" value="Registrate">
-        </form>
-      </td>
-      <td>
-        <form action="login.php">
-          <input type="submit" id="login2" name="login" value="Iniciar Sesión">
-        </form>
-      </td>
-    </tr>
-  </header>
-  <header id="position3">
-    <!--moto C, en vertical-->
-    <form action="index_search.php" method="get">
-      <input type="search" id="busqueda3" name="busqueda" required placeholder="¿Que artículo buscas?">
-      <input type="submit" id="enviar3" name="enviar" value="Buscar">
-    </form>
-  </header>
-  <br><br><br>
-  <!--Aqui esta la presentacion-->
-  <div class="logo"><img src="imagenes/imagenes_chalalas/chalalas2.gif" alt="chalalas.com"></div>
-  <main class="contenido-principal">
-    <h1 class="construccion">Sitio en construcción, proximamente...</h1>
-    <p class="contenido_resumen">
-      Sitio web de comercio electronico, aqui podras encontrar o vender ese articulo que necesitas ya sea nuevo o usado
-      y cerca de ti de persona a persona.
+<?php include("template/presentacion.php"); //bienvenido
+?>
 
-      Registrate para recibir notificaciones de lo que llegues estar buscando que no existe o para esa compra/venta de
-      oportunidad.
+<?php //include("carrusel.php"); //cambiar nombre de clases para poder usar
+?>
 
-  </main>
 
-  <!--Galeria-->
-  <div class="row">
-    <div class="column">
-      <a href="carrusel2.html"><img class="galeria" alt="photo 1" src="img/audifonos bluetooh.jpg" />
-        <a href="proximamente.php"> <img class="galeria" alt="photo 2" src="img/cargador_sony_1.jpg" />
-          <a href="proximamente.php"> <img class="galeria" alt="photo 3" src="img/IMG_20221112_111654_742.jpg" />
-            <a href="proximamente.php"> <img class="galeria" alt="photo 4" src="img/IMG_20230404_173830_587.jpg" />
-              <a href="proximamente.php"> <img class="galeria" alt="photo 5" src="img/IMG_20230411_020301_788.jpg" />
-    </div>
+<?php include("users/template/galeria1.php"); //Las imagenes 
+?>
 
-    <div class="column">
-      <a href="proximamente.php"> <img class="galeria" alt="photo 6" src="img/IMG_20230417_002758_978.jpg" />
-        <a href="proximamente.php"> <img class="galeria" alt="photo 7" src="img/IMG_20230417_003024_468.jpg" />
-          <a href="proximamente.php"> <img class="galeria" alt="photo 8" src="img/IMG_20230421_170947_190.jpg" />
-            <a href="proximamente.php"> <img class="galeria" alt="photo 9" src="img/IMG_20230423_175747_290.jpg" />
-              <a href="proximamente.php"> <img class="galeria" alt="photo 10" src="img/IMG_20230429_123940_723.jpg" />
-    </div>
 
-    <div class="column">
-      <a href="proximamente.php"> <img class="galeria" alt="photo 11" src="img/IMG_20230523_170746_823.jpg" />
-        <a href="proximamente.php"> <img class="galeria" alt="photo 12" src="img/IMG_20230531_003537_082.jpg" />
-          <a href="proximamente.php"> <img class="galeria" alt="photo 13" src="img/IMG_20230603_023752_895.jpg" />
-            <a href="proximamente.php"> <img class="galeria" alt="photo 14" src="img/IMG_20230626_201617_060.jpg" />
-              <a href="proximamente.php"> <img class="galeria" alt="photo 15" src="img/lampara_amarilla_3.jpg" />
-    </div>
-
-    <div class="column">
-      <a href="proximamente.php"> <img class="galeria" alt="photo 16" src="img/marcador_verde.jpg" />
-        <a href="proximamente.php"> <img class="galeria" alt="photo 17"
-            src="img/Marzo 2023 Zona de trabajo Yanni.jpg" />
-          <a href="proximamente.php"> <img class="galeria" alt="photo 18" src="img/Screenshot_20230305-153431.png" />
-            <a href="proximamente.php"> <img class="galeria" alt="photo 19" src="img/Screenshot_20230324-190323.png" />
-              <a href="proximamente.php"><img class="galeria" alt="photo 20"
-                  src="img/vasos de cafe desechables.jpg" /></a>
-    </div>
-  </div>
-
-  <br>
-  <a aria-label="Chat on WhatsApp" href="https://wa.me/5517602354"><img class="whatsapp" alt="Chat on WhatsApp"
-      src="WhatsAppButtonGreenSmall.png" />
-  </a>
-</body>
-
-</html>
+<?php include("template/piemenu.php"); //Pendiente:reglas i condiciones de uso, soporte. etc.
+?>
