30b012880a1eb13c9ba375cad5da7eb33b18c417
Yanni Castle <yannicastle5@gmail.com>
2024-05-05 02:31:41 -0600

    get en index

--- index.php
+++ index.php
@@ -1,17 +1,13 @@
 <?php include("template/cabeceramenu.php"); //Aqui esta el buscador y el menu
 ?>
 
+<?php
+if (!isset($_GET['enviar'])) {
+  include("template/presentacion.php"); //bienvenido
 
-<?php include("template/presentacion.php"); //bienvenido
+  include("users/template/galeria1.php"); //Las imagenes 
+}
 ?>
-
-<?php //include("carrusel.php"); //cambiar nombre de clases para poder usar
-?>
-
-
-<?php include("users/template/galeria1.php"); //Las imagenes 
-?>
-
 
 <?php include("template/piemenu.php"); //Pendiente:reglas i condiciones de uso, soporte. etc.
 ?>
