a4b4b884089fd0999f2468371c0322b9a464fae6
YanniCastle <yannicastle5@gmail.com>
2025-01-20 20:11:59 -0600

    eliminar texto

---
+++ eliminar_texto.php
@@ -0,0 +1,15 @@
+<?php
+include 'config.php';
+if (isset($_POST["eliminar_texto"])) {
+  
+  $id = 0;
+  $id = $_GET['id'];
+  $id_texto = $_GET['id_texto'];
+  $sql = "DELETE FROM comentarios WHERE ID_USER =$id  AND  Id =$id_texto ";
+  $res = mysqli_query($con2, $sql);
+  if ($res) {
+    echo '<script>alert("Eliminado Correctamente"); window.location="Formulario.php";</script>';
+  }
+} else {
+  echo "No se pudo eliminar el texto.";
+}
