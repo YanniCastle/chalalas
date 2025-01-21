ea3e114e1a9fcbdaa8664732e5680c4f039f8799
YanniCastle <yannicastle5@gmail.com>
2025-01-20 20:00:08 -0600

    videos-no

--- template/cabeceramenu.php
+++ template/cabeceramenu.php
@@ -36,7 +36,6 @@
       <nav class="menu">
       <a href="ver_comentarios.php">Comentarios<i class="fa-regular fa-comment"></i></a>
         <a href="ofertas.php">Ofertas<i class="fa-regular fa-image"></i></a>
-        <a href="">videos<i class="fa-solid fa-video"></i></a>
         <a href="">Lo nuevo<i class="fa-solid fa-person-through-window"></i></a>
         <a href="Formulario_Insertar_Usuarios3.php">Registrate</a>
         <a href="login.php">iniciar sesión</a>
